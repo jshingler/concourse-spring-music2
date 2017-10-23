3d8b1906acb507eec99368b0690c0210ac1b2609
James Shingler <James.Shingler@CardinalHealth.com>
2017-10-23 19:47:17 -0400

    remove version requirement

--- ci/scripts/build-artifact.sh
+++ ci/scripts/build-artifact.sh
@@ -2,7 +2,8 @@
 set -e
 
 export GRADLE_OPTS=-Dorg.gradle.native=false
-version=`cat version/number`
+# version=`cat version/number`
+version=1.0.1
 cd git-repo
 gradle assemble -Pversion=$version
 cp build/libs/*.jar ../artifact-dir/
