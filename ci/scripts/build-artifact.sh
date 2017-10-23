4ac6d0f10d4e75163d80511745fba1ded196a680
James Shingler <James.Shingler@CardinalHealth.com>
2017-10-23 19:31:06 -0400

    Tyring out maven-resource

--- ci/scripts/build-artifact.sh
+++ ci/scripts/build-artifact.sh
@@ -5,5 +5,5 @@ export GRADLE_OPTS=-Dorg.gradle.native=false
 version=`cat version/number`
 cd git-repo
 gradle assemble -Pversion=$version
-cp build/libs/*.war ../artifact-dir/
+cp build/libs/*.jar ../artifact-dir/
 ls ../artifact-dir
