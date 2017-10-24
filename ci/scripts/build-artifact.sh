bb365421eb902c511aada694b3d1275b2e501d02
James Shingler <James.Shingler@CardinalHealth.com>
2017-10-23 21:50:49 -0400

    working on snapshot

--- ci/scripts/build-artifact.sh
+++ ci/scripts/build-artifact.sh
@@ -3,7 +3,7 @@ set -e
 
 export GRADLE_OPTS=-Dorg.gradle.native=false
 # version=`cat version/number`
-version=1.0.1
+version=1.0.1-SNAPSHOT
 cd git-repo
 gradle jar -Pversion=$version
 cp build/libs/*.jar ../artifact-dir/
